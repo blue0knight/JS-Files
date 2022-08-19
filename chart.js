6d080e48f7fbdf2b539dacebd613b04c40d46b2d
Anthony <oni_rivera98@yahoo.com>
2022-08-19 16:21:01 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,20 +1,20 @@
-function insertChart () {    
-        new Chart(document.getElementById("myChart"), {
-    type: 'pie',
-    data: {
-      labels: ["Books and Materials","Sedgwick Tuition Assistance Program", "Non-Education Related Costs"],
-      datasets: [{
-        label: "Percentage",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
-        data: [60,5,35]
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'How Your Full Tuition Grant Works, %'
-      }
-    }
-});
-    }
+function insertChart() {
+    new Chart(document.getElementById("myChart"), {
+        type: 'pie',
+        data: {
+            labels: ["Books and Materials", "Sedgwick Tuition Assistance Program", "Non-Education Related Costs"],
+            datasets: [{
+                label: "Percentage",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+                data: [60, 5, 35]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'How Your Full Tuition Grant Works, %'
+            }
+        }
+    });
+}
 insertChart();
